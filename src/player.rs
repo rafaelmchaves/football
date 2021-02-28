6b6540a05e46db3222222374562175dbb652f030
Rafael Chaves <rafael.chaves@airfox.com>
2021-02-28 12:29:20 -0300

    Create player and team structs

---
+++ src/player.rs
@@ -0,0 +1,7 @@
+struct player {
+  id: String,
+  name: String,
+  age: u8,
+  main_position: String,
+  nation: String,
+}
---
+++ src/team.rs
@@ -0,0 +1,10 @@
+struct Team {
+  id: String,
+  name: String,
+  color_1: String,
+  color_2: String,
+  stadium: String,
+  president: String,
+  nation: String,
+
+}
